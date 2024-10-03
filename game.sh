68714af12b2a0fabf438b744a8080672b8c02db7
Fizza A <fahmed@smail.pembinatrails.ca>
2024-10-03 13:40:06 -0500

    Add interactive prompts to game script

--- game.sh
+++ game.sh
@@ -1,6 +1,11 @@
+<<<<<<< HEAD
 
 echo "Welcome to the Ultimate Adventure Game with Monsters!"
 echo "Welcome to the Adventure Game!" echo "Choose a location to explore: forest, castle, or cave."
+=======
+echo "Welcome to the Adventure Game!"
+echo "Choose a location to explore: forest, castle, or cave."
+>>>>>>> 84e82ed (Add interactive prompts to game script)
 read location
 if ["$location" == "forest" ]; then
   cat forest.txt
@@ -8,8 +13,11 @@ elif ["$location" == "castle"]; then
   cat castle.txt
 elif ["$location" == "cave"]; then
   cat cave.txt
+<<<<<<< HEAD
 echo "But wait...
 cat monster.txt
+=======
+>>>>>>> 84e82ed (Add interactive prompts to game script)
 else
 echo "Invalid location."
 fi
